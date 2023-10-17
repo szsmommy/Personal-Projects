160afab1d2388d1bf7a0b35f69f8181c0241d7c6
szsmommy <szsmommy@gmail.com>
2023-10-17 08:30:23 -0700

    Edited contact-form-validation.js and index.js to show when inputs are correct

--- contact-form-validation.js
+++ contact-form-validation.js
@@ -1,33 +1,61 @@
-function validateForm() {
-    let x = document.forms["myForm"]["fname"]["phnum"]["email"].value;
-    if (x == "") {
-      alert("Name must be filled out");
-      return false;
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.forms["myForm"];
+    const fnameInput = form["fname"];
+    const phnumInput = form["phnum"];
+    const emailInput = form["email"];
+    const submitBtn = form.querySelector('input[type="submit"]');
+    const successMessage = document.getElementById("successMessage");
+​
+    function validateForm(event) {
+        let isValid = true;
+​
+        if (fnameInput.value.trim() === "") {
+            fnameInput.style.borderColor = "red";
+            isValid = false;
+        } else {
+            fnameInput.style.borderColor = "";
+        }
+​
+        if (!phnumInput.checkValidity()) {
+            phnumInput.style.borderColor = "red";
+            isValid = false;
+        } else {
+            phnumInput.style.borderColor = "";
+        }
+​
+        if (!isEmailValid(emailInput.value)) {
+            emailInput.style.borderColor = "red";
+            isValid = false;
+        } else {
+            emailInput.style.borderColor = "";
+        }
+​
+        if (isValid) {
+            successMessage.style.display = "block";
+        } else {
+            successMessage.style.display = "none";
+        }
+​
+        return isValid;
     }
-  }
-
-  const isValid = (usernameInputValue) => {
-
-    if (!validator.isEmpty(usernameInputValue) && validator.isLength(usernameInputValue, { min: 5 })) {
-    
-    return {
-    
-    valid: true,
-    
-    msg: null
+​
+    function handleFormSubmit(event) {
+        if (!validateForm()) {
+            event.preventDefault();
+        }
     }
-    
- } else {  
-    if (validator.isEmpty(fname)) {
-      return {
-        valid: false,
-        msg: "Required"
-      }
-   
-    } else {
-      return {
-        valid: false,
-        msg: "Input invalid"
-      }
+​
+    function isEmailValid(email) {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
     }
-    
+​
+    form.addEventListener("submit", handleFormSubmit);
+​
+    form.addEventListener("input", function() {
+        if (validateForm()) {
+            submitBtn.removeAttribute("disabled");
+        } else {
+            submitBtn.setAttribute("disabled", "disabled");
+        }
+    });
+});
