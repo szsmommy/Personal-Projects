dfecdc4bded769c0f789146938b4971aa1731e42
szsmommy <szsmommy@gmail.com>
2023-10-17 12:02:48 -0700

    Completed edits on contanct form validation

--- js/contact-form-validation2.js
+++ js/contact-form-validation2.js
@@ -5,52 +5,52 @@ document.addEventListener("DOMContentLoaded", function() {
     const emailInput = form["email"];
     const submitBtn = form.querySelector('input[type="submit"]');
     const successMessage = document.getElementById("successMessage");
-​
+
     function validateForm(event) {
         let isValid = true;
-​
+
         if (fnameInput.value.trim() === "") {
             fnameInput.style.borderColor = "red";
             isValid = false;
         } else {
             fnameInput.style.borderColor = "";
         }
-​
+
         if (!phnumInput.checkValidity()) {
             phnumInput.style.borderColor = "red";
             isValid = false;
         } else {
             phnumInput.style.borderColor = "";
         }
-​
+
         if (!isEmailValid(emailInput.value)) {
             emailInput.style.borderColor = "red";
             isValid = false;
         } else {
             emailInput.style.borderColor = "";
         }
-​
+
         if (isValid) {
             successMessage.style.display = "block";
         } else {
             successMessage.style.display = "none";
         }
-​
+
         return isValid;
     }
-​
+
     function handleFormSubmit(event) {
         if (!validateForm()) {
             event.preventDefault();
         }
     }
-​
+
     function isEmailValid(email) {
         return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
     }
-​
+
     form.addEventListener("submit", handleFormSubmit);
-​
+
     form.addEventListener("input", function() {
         if (validateForm()) {
             submitBtn.removeAttribute("disabled");
